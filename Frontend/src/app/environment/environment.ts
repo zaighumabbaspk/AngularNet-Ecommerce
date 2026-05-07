3612193ad2a211488b453abdeb48879db40fbdf0
Zaighum Abbas <zaighumabbas786000@gmail.com>
2026-05-07 17:54:09 +0500

    Update API URL to Azure VM

--- Frontend/src/app/environment/environment.ts
+++ Frontend/src/app/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:7137',
-  apiBaseUrl: 'https://localhost:7137/api',
+  apiUrl: 'http://20.197.50.132',
+  apiBaseUrl: 'http://20.197.50.132/api',
   stripePublishableKey: 'pk_test_51T9hpvJ31GzrQr6WvvmwnycLI02XAXiSyyz97ytmguN2ZhbAo57i2xTCTcc7Ip3xt5Oqe3LxwhaDbOyV0zcEo8IS00zVsClpRz'
 };
